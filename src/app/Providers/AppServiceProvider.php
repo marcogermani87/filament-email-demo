ab0018caa5b9010fec588ff743f76ec6cf396b43
Marco Germani <info@marcogermani.it>
2024-07-02 20:26:59 +0200

    Bump filamen-email to v1.4.8. Updated deps.

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -27,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
         }
         LanguageSwitch::configureUsing(function (LanguageSwitch $switch) {
             $switch
-                ->locales(['en', 'nl', 'it']);
+                ->locales(['en', 'nl', 'pt', 'it']);
         });
     }
 }
